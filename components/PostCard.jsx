efc0dd7668b15a9a9e4b071bd68c8229fca515bd
Dilshodbek <dilshodjongulomov53@gmail.com>
2023-08-02 13:15:09 +0500

    changes in style

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const PostCard = ({ post }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+    <div className="bg-white shadow-lg rounded-lg p-2 lg:p-8 pb-12 mb-8">
       <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-indigo-700 text-3xl font-semibold">
         <Link href={`/post/${post.slug}`}>{post.title}</Link>
       </h1>
